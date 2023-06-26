dcbd652d2e5443d320e97261942af9e7d7aafbfd
jmoua123 <nraugnaagmouaa@gmail.com>
2023-06-26 15:14:49 -0700

    init learning

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+#this module is responsible for requests or this is also called a request handler 
+
+#this processes html files
+from django.shortcuts import render 
+#this processes api requests
 from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('Hello World')
+    return render(request, 'hello.html', {'name' : 'Jonah'})
